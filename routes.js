652132725c8dab09f6c48d58f7728a1347fb8909
Justin Zimmer <jtzimmer6@gmail.com>
2023-08-07 13:40:39 -0700

    Add GET routes

--- routes.js
+++ routes.js
@@ -2,7 +2,51 @@ const { storeTransactACH, storeTransactCC } = require('./mssql-datasource');
 const { validateCreditCardNumber, validateABARoutingNumber, checkAPIKey } = require('./helpers')
 function routes(fastify, options, done) {
 
-  // Credit Card Transaction Route
+  // Credit Card Transaction Routes
+  fastify.get('/api/transactions/creditcard', {
+    schema: {
+      querystring: {
+        type: 'object',
+        properties: {
+          phoneNo: { type: 'string' },
+          accountNo: { type: 'string' },
+          leadID: { type: 'string' },
+          creditCardNo: { type: 'string' },
+          creditCardExp: { type: 'string' },
+          creditCardSec: { type: 'string' },
+        },
+        required: ['phoneNo', 'accountNo', 'leadID', 'creditCardNo', 'creditCardExp', 'creditCardSec']
+      }
+    },
+  }, async (request, reply) => {
+    // Secure Route: Check API Key
+    checkAPIKey(request, reply)
+    // Implementation of your route
+    const {
+      phoneNo,
+      accountNo,
+      leadID,
+      creditCardNo,
+      creditCardExp,
+      creditCardSec
+    } = request.query;
+  
+    // Verify Credit Card Number
+    if (!validateCreditCardNumber(creditCardNo)) {
+      reply.code(400).send({ message: 'Invalid Credit Card Number' });
+      return;
+    }
+  
+    try {
+      await storeTransactCC(phoneNo, accountNo, leadID, creditCardNo, creditCardExp, creditCardSec)
+      // Depending on your stored procedure's response, you may want to send specific messages or status codes
+      reply.send({ message: 'Transaction stored successfully.' });
+      
+    } catch (err) {
+      reply.code(500).send({ message: 'Internal Server Error', error: err });
+    }
+  });
+  
   fastify.post('/api/transactions/creditcard', {
     schema: {
       body: {
@@ -47,7 +91,48 @@ function routes(fastify, options, done) {
     }
   });
 
-  // ACH Transaction Route
+  // ACH Transaction Routes
+  fastify.get('/api/transactions/ach', {
+    schema: {
+      querystring: {
+        type: 'object',
+        properties: {
+          phoneNo: { type: 'string' },
+          accountNo: { type: 'string' },
+          leadID: { type: 'string' },
+          achRoutingNo: { type: 'string' },
+          achAccountNo: { type: 'string' },
+        },
+        required: ['phoneNo', 'accountNo', 'leadID', 'achRoutingNo', 'achAccountNo']
+      }
+    },
+  }, async (request, reply) => {
+    // Secure Route: Check API Key
+    checkAPIKey(request, reply)
+    // Implementation of your route
+    const {
+      phoneNo,
+      accountNo,
+      leadID,
+      achRoutingNo,
+      achAccountNo,
+    } = request.query;
+  
+    // Verify routing Number
+    if (!validateABARoutingNumber(achRoutingNo)) {
+      reply.code(400).send({ message: 'Invalid Routing Number' });
+      return;
+    }
+  
+    try {
+      await storeTransactACH(phoneNo, accountNo, leadID, achRoutingNo, achAccountNo)
+      reply.send({ message: 'Transaction stored successfully.' });
+
+    } catch (err) {
+      reply.code(500).send({ message: 'Internal Server Error', error: err });
+    }
+  });
+  
   fastify.post('/api/transactions/ach', {
     schema: {
       body: {
